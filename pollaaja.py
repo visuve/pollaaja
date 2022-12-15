35c4562fbc8b970260cf749996ce7ec99104d68a
visuve <veli-matti.visuri@iki.fi>
2022-12-15 17:03:03 +0200

    Add SMTP notifications
    - TODO: untested!

--- pollaaja.py
+++ pollaaja.py
@@ -5,14 +5,32 @@ from datetime import datetime
 import argparse
 import requests
 import time
+import smtplib
+import ssl
+
+MESSAGE = """From: pollaaja@peelo.net
+To: {0}
+Content-Type: text/plain; charset=utf-8
+Subject: Notification!
+
+The text "{1}" can be found from {2}.
+"""
 
 class Pollaaja():
-    def __init__(self, url, text):
+    def __init__(self, url, text, host, port, username, password, recipient):
         self.url = url
         self.text = text
+        self.recipient = recipient
+        self.message = MESSAGE.format(recipient, text, url).encode("utf-8")
+
+        self.ctx = ssl.create_default_context()
+        self.server = smtplib.SMTP_SSL(host, port, self.ctx)
+        self.server.login(username, password)
+
+    def __del__(self):
+        self.server.quit()
 
     def run(self):
-
         found = False
 
         while not found:
@@ -29,7 +47,10 @@ class Pollaaja():
             found = response.text.find(self.text) >= 0
 
             if found:
-                print("Huzzah!")
+                self.server.sendmail(
+                    "pollaaja@peelo.net",
+                    [self.recipient],
+                    self.message)
                 break
             else:
                 print("The text is not found yet!")
@@ -42,7 +63,21 @@ if __name__ == "__main__":
     parser.add_argument("-w", "--website", type=str, required=True, help="website to poll")
     parser.add_argument("-t", "--text", type=str, required=True, help="text to poll")
 
+    parser.add_argument("-s", "--server", type=str, required=True, help="SMTP host/IP to connect")
+    parser.add_argument("-p", "--port", type=str, required=False, help="SMTP port to connect", default=587)
+
+    parser.add_argument("-u", "--username", type=str, required=True, help="SMTP user")
+
+    parser.add_argument("-r", "--recipient", type=str, required=True, help="notification email recipient")
+
     args = parser.parse_args()
 
-    pollaaja = Pollaaja(args.website, args.text)
+    pollaaja = Pollaaja(
+        args.website,
+        args.text,
+        args.host,
+        args.port,
+        args.username,
+        input("Type your password and press enter: "),
+        args.recipient)
     pollaaja.run()
